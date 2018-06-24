ad0a2ca95d684161131f33511bbeaa5b991fb17d
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-06-24 02:59:37 -0700

    Cleaned Out Sensors List

--- app/src/main/java/com/beyondbell/multility/sensors/AvailableSensorsList.kt
+++ app/src/main/java/com/beyondbell/multility/sensors/AvailableSensorsList.kt
@@ -3,18 +3,7 @@ package com.beyondbell.multility.sensors
 object AvailableSensorsList {
     val list = ArrayList<Sensors>()
 
-    enum class Sensors(check: Boolean, init: Unit) {
-        Compass(compassCheck(), compassInit());
-
-        val check = check
-        val init = init
-    }
-
-    fun compassCheck() : Boolean {
-        return true
-    }
-
-    fun compassInit() : Unit {
-        return true
+    enum class Sensors(val check: Boolean, val init: Unit) {
+        // TODO Populate
     }
 }
