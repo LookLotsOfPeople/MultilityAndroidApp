d3b4ce77b45de76b054c6356c2214523e55dbee4
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-06-24 02:50:32 -0700

    Landing Page Finish

--- app/src/main/java/com/beyondbell/multility/LandingScreen.kt
+++ app/src/main/java/com/beyondbell/multility/LandingScreen.kt
@@ -1,34 +1,39 @@
 package com.beyondbell.multility
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.beyondbell.multility.sensors.AvailableSensorsList
 
 class LandingScreen : AppCompatActivity() {
+    private val setupThreads = ArrayList<Thread>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.landing)
         checkSensors()
+        waitForSetup()
         openMainMenu()
     }
 
     private fun checkSensors() {
         for (sensor in AvailableSensorsList.Sensors.values()) {
-            if (true /*Has Sensor*/) {
+            if (sensor.check) {
                 AvailableSensorsList.list.add(sensor)
-                Runnable {
-                    loadModule(sensor)
-                }.run()
+                val setupThread = Thread(sensor::init)
+                setupThreads.add(setupThread)
+                setupThread.start()
             }
         }
     }
 
-    private fun loadModule(sensors: AvailableSensorsList.Sensors) {
-        // TODO Any Pre-Loading
+    private fun waitForSetup() {
+        for (setupThread in setupThreads) {
+            setupThread.join()
+        }
     }
 
     private fun openMainMenu() {
-
+        this@LandingScreen.startActivity(Intent(this@LandingScreen, MainMenu::class.java))
     }
 }
--- app/src/main/java/com/beyondbell/multility/sensors/AvailableSensorsList.kt
+++ app/src/main/java/com/beyondbell/multility/sensors/AvailableSensorsList.kt
@@ -3,7 +3,18 @@ package com.beyondbell.multility.sensors
 object AvailableSensorsList {
     val list = ArrayList<Sensors>()
 
-    enum class Sensors {
-        Compass,
+    enum class Sensors(check: Boolean, init: Unit) {
+        Compass(compassCheck(), compassInit());
+
+        val check = check
+        val init = init
+    }
+
+    fun compassCheck() : Boolean {
+        return true
+    }
+
+    fun compassInit() : Unit {
+        return true
     }
 }
