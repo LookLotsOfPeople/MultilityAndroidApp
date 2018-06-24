0ff36ea22a20c1c4e813ebdc62b210beca2aaba9
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-06-24 03:28:36 -0700

    Removed Test

--- app/src/main/java/com/beyondbell/multility/utilities/Calculator.kt
+++ app/src/main/java/com/beyondbell/multility/utilities/Calculator.kt
@@ -2,18 +2,11 @@ package com.beyondbell.multility.utilities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.beyondbell.multility.R
-import kotlinx.android.synthetic.main.calculator.*
 
 class Calculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calculator)
-        val bttn = button
-        button.setOnClickListener {
-            System.out.print("HELLO")
-            Log.i("Calculator", "HELLO")
-        }
     }
 }
