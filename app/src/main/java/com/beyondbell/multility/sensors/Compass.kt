c6f8cbc7eb3273d63261432b0383bb8cee60e293
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-06-24 21:38:16 -0700

    Sensor Stuff

---
+++ app/src/main/java/com/beyondbell/multility/sensors/Compass.kt
@@ -0,0 +1,16 @@
+package com.beyondbell.multility.sensors
+
+import android.view.View
+
+class Compass : SensorView() {
+
+    override fun getSensorName(): String {
+        return "Compass"
+    }
+
+    override fun getSensorView(): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
--- app/src/main/java/com/beyondbell/multility/sensors/SensorView.kt
+++ app/src/main/java/com/beyondbell/multility/sensors/SensorView.kt
@@ -1,7 +1,29 @@
 package com.beyondbell.multility.sensors
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.beyondbell.multility.R
+import kotlinx.android.synthetic.main.sensor.*
 
-class SensorView : AppCompatActivity() {
+abstract class SensorView : AppCompatActivity() {
+    abstract fun getSensorName() : String
+    abstract fun getSensorView() : View
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sensor)
+
+        setupVisuals()
+        initSubview()
+    }
+
+    private fun setupVisuals() {
+        sensorName.text = getSensorName()
+    }
+
+    private fun initSubview() {
+
+    }
 }
