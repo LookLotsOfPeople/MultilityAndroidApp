952e00f12c252fbbd04e254ad9ebd12d82088bf7
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-06-26 16:44:02 -0700

    Utalize Action Bar

--- app/src/main/java/com/beyondbell/multility/ActionBarActivity.kt
+++ app/src/main/java/com/beyondbell/multility/ActionBarActivity.kt
@@ -1,15 +1,38 @@
 package com.beyondbell.multility
 
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 
 abstract class ActionBarActivity : AppCompatActivity() {
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		val inflater = menuInflater
+		inflater.inflate(R.menu.action_bar, menu)
+		return true
+	}
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setupActionBar()
 	}
 
 	private fun setupActionBar() {
-		
+		val actionBar : ActionBar = supportActionBar!!
+		actionBar.title = getString(R.string.app_name)
+		actionBar.subtitle = getString(R.string.app_desc)
+	}
+
+	fun openOptions(menuItem: MenuItem?) {
+
+	}
+
+	fun quitApplication(menuItem: MenuItem?) {
+		finishAndRemoveTask()
+	}
+
+	fun openSensor(menuItem: MenuItem?) {
+
 	}
 }
